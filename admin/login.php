ce7b9e6aff1ba5db2ec82adbe1155aea11f0a3aa
Isaac Nyamari <jablessions76@gmail.com>
2024-12-19 17:07:00 +0300

    updated

---
+++ admin/login.php
@@ -0,0 +1,24 @@
+<?php include("header.php") ?>
+
+<body>
+    <div class="login-container">
+        <div class="form">
+            <h2 class="text-start text-danger"><i class="fa fa-user"></i> Login</h2>
+            <form action="">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+                <div class="form-group mt-2">
+                    <button class="btn btn-secondary"><i class="fa fa-sign-in"></i> Sign in</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
